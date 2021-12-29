4ca78fb2316ba6259b7969ea75023d7e0c28651a
ElonGates <59313695+VilotStar@users.noreply.github.com>
2021-12-29 02:42:47 +0000

    English Fix

--- src/views/Artwork/index.vue
+++ src/views/Artwork/index.vue
@@ -24,8 +24,8 @@
       v-model="ugoiraDownloadPanelShow"
       :actions="ugoiraDownloadPanelActions"
       @select="onUgoiraDownloadPanelSelect"
-      cancel-text="取消"
-      description="请选择下载格式"
+      cancel-text="Cancel"
+      description="Select The Download Format"
       close-on-popstate
       close-on-click-action
     />
@@ -68,9 +68,9 @@ export default {
       },
       ugoiraDownloadPanelShow: false,
       ugoiraDownloadPanelActions: [
-        { name: "ZIP", subname: "原始序列帧归档文件" },
-        { name: "GIF", subname: "低画质，兼容性最佳" },
-        { name: "WebM", subname: "高画质，兼容性差" } // chrome only
+        { name: "ZIP", subname: "Archive" },
+        { name: "GIF", subname: "Low Image Quality" },
+        { name: "WebM", subname: "High picture quality" } // chrome only
       ]
     };
   },
@@ -97,7 +97,7 @@ export default {
 
         if (this.isCensored(this.artwork)) {
           this.$toast({
-            message: "根据当前设置，此内容将不予显示",
+            message: "The Settings Disallow This Content",
             icon: require("@/svg/ban-view.svg")
           });
           setTimeout(() => {
